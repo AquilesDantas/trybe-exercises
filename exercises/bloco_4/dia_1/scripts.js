fe94e3b83277f0c6156368f822faec07cb5094e2
AquilesDantas <aquilesdant@gmail.com>
2021-05-19 20:16:05 -0300

    Adiciona exercicio do bloco 4.1

---
+++ exercises/bloco_4/dia_1/scripts.js
@@ -0,0 +1,13 @@
+
+let selecao = 'Aprovada';
+
+switch (selecao) {
+    case 'Aprovada':
+        console.log('Parabéns, vocẽ foi aprovado(a)!');
+        break;
+    case 'lista':
+        console.log('Vocẽ está em nossa lista de espera');
+        break;
+    default:
+        console.log('Você foi reprovado(a)');
+}
