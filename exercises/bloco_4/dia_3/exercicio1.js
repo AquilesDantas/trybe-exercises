eae6148e61b403428061a5da42123b6c79fb2ae5
Aquiles Dantas <aquilesdant@gmail.com>
2021-05-21 20:25:42 -0300

    Realizados exercicios 1,2 e 3

---
+++ exercises/bloco_4/dia_3/exercicio1.js
@@ -0,0 +1,15 @@
+
+let asterisco = "*";
+let coluna = 5;
+let linha = "";
+ 
+for (index = 1; index <= coluna; index += 1) {
+    let linha = "";
+  for(indexrep =1; indexrep <= coluna; indexrep += 1) {
+    
+        linha = linha + asterisco;
+       
+        
+    } console.log(linha);
+
+}
