6023def1ae949ca6f4ef9f2938094e08937cc4c9
Aquiles Dantas <aquilesdant@gmail.com>
2021-05-26 15:53:23 -0300

    Adcao de arquivo para exercicios, testes e projetos

---
+++ test.js
@@ -0,0 +1,7 @@
+function techList () {
+    let nomesTech = ['React', 'Python','DOM','SQL']
+    let name = 'Aquiles'
+    nomesTech.sort();
+    return nomesTech.sort();
+
+} console.log (techList)
