5617f17a8763ec2e692c832efa6330842cbb80f6
Aquiles Dantas <aquilesdant@gmail.com>
2021-07-28 16:47:22 -0300

    Exercicios 8.1/8.2

--- exercises/8.1/script.js
+++ exercises/8.1/script.js
@@ -1,4 +1,4 @@
-const pessoa = {
+/*const pessoa = {
   p1:'pedro',
   p2:'joao',
   p3:'tiago',
@@ -28,18 +28,18 @@ const newEmployees = (nomes) => {
   return employees;
 };
 
-/*1 - Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada.
+/* 1 - Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada.
 Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id .
 A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente
 um email no formato nome_da_pessoa@trybe.com.*/
-console.log(nomes(pessoa,email));
+// console.log(nomes(pessoa,email));
 
 
 // 2 - Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5.
 // recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado.
 // O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
 
-const aposta = [1,2,3,4,5];
+/*const aposta = [1,2,3,4,5];
 
 const getRandomInt = (min, max) => {
   let arrayresult = [];
@@ -57,6 +57,30 @@ if(resultado === aposta) {
   }
 };
 
-console.log(getRandomInt(0,9));
+console.log(getRandomInt(0,9)); */
+
+// 3-----------------------------------------------
+// 3 - Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito),
+//  o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro
+//  é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida
+//  pela pessoa estudante. Ao final a HOF deve retornar o total da contagem de respostas certas.
+// Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos,
+//  e quando não houver resposta ("N.A") não altera-se a contagem.
 
 
+const  armazena = () => {
+  const rightAnswers = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+  const ciclo = [];
+  for (let index = 0; index < rightAnswers.length; index++) {
+    ciclo = rightAnswers[index];
+    return compara(ciclo);
+    
+  }
+}
+const compara = (ciclo) => {
+  const studentAnswers = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+  for (let index = 0; index < studentAnswers.length; index++) {
+    
+    
+  }
+}
