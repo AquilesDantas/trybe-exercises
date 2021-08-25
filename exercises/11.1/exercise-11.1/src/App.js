f92247246902b378505fb2c13e0d119203699dcb
AquilesDantas <aquilesdant@gmail.com>
2021-08-25 15:59:50 -0300

    Exercicios 11.1

---
+++ exercises/11.1/exercise-11.1/src/App.js
@@ -0,0 +1,54 @@
+import './App.css';
+import ReactDOM from 'react-dom';
+
+// const appointments = [
+//   'Arrumar a casa',
+//   'Dar banho no cachorro',
+//   'Lavar o quintal',
+//   'Ir ao mercado'
+// ]
+
+// const Task = (value) => {
+//   return (
+//    <ul>{appointments.map((appointment) => <li>{appointment}</li>)}</ul>
+//   );
+// }
+
+// const Jsx = () => {
+//   return (
+//     <h1>Hello, JSX!</h1>
+//   );
+// }
+// const ComplexJSX = () => {
+//   return (
+//     <div className="myDiv">
+// {/* Lorem Ipsum is simply dummy text */}
+//     <h1>Lorem Ipsum</h1>
+//     <p>Lorem Ipsum</p>
+//     <ul>
+//       <li>is simply dummy text</li>
+//       <li>is simply dummy text</li>
+//       <li>is simply dummy text</li>
+//     </ul>
+//     </div>
+//   );
+// }
+// ReactDOM.render(
+//   <div>
+//     <h1>Lorem Ipsum</h1>
+//     <p>Lorem Ipsum</p>
+//   </div>,
+//     document.getElementById("root")
+// )
+    // <div>
+    //   <br />
+    //   <hr />
+    // </div>
+
+function App() {
+  return (
+    <div>Oi</div>
+  );    
+}
+
+export default App;
